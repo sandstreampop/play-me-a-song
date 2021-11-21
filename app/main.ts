4671fa594eaa795f22a6bca0d018312704083002
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-11-21 13:20:43 +0100

    feat: bundled js

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+if (ctx) {
+	ctx.fillStyle = 'green';
+	ctx.fillRect(10, 10, 150, 100);
+}
