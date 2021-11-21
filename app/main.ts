6121ef9d2268654a0d092b8315e5a010527fd59a
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-11-21 14:04:48 +0100

    feat: resizing canvas

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,29 @@
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
 
-if (ctx) {
-	ctx.fillStyle = 'green';
-	ctx.fillRect(10, 10, 150, 100);
+addEventListener("resize", resizeCanvas, false);
+
+function resizeCanvas() {
+  if (!isCanvas(canvas)) return;
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  draw(canvas);
+}
+
+resizeCanvas();
+
+function draw(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d");
+
+  if (ctx === null) return;
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(10, 10, 150, 100);
+}
+
+
+// deno-lint-ignore no-explicit-any
+function isCanvas(maybeCanvas: any): maybeCanvas is HTMLCanvasElement {
+  if (maybeCanvas === undefined) return false;
+  return typeof maybeCanvas.getContext === "function";
 }
--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-if (ctx) {
-    ctx.fillStyle = 'green';
+const canvas = document.getElementById("canvas");
+addEventListener("resize", resizeCanvas, false);
+function resizeCanvas() {
+    if (!isCanvas(canvas)) return;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw(canvas);
+}
+resizeCanvas();
+function draw(canvas) {
+    const ctx = canvas.getContext("2d");
+    if (ctx === null) return;
+    ctx.fillStyle = "green";
     ctx.fillRect(10, 10, 150, 100);
+}
+function isCanvas(maybeCanvas) {
+    if (maybeCanvas === undefined) return false;
+    return typeof maybeCanvas.getContext === "function";
 }
 
